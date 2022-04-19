19858c7808be159c4db800c92d0a5bd1e32430d7
di8ry <d.nosov1005@mail.ru>
2022-04-19 19:11:31 +0700

    add brain_even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from brain_games.games.even import get_question_and_answer
+
+
+def main():
+    get_question_and_answer()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
