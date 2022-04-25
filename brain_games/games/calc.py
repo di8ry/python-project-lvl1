58fdd4166819e1dceddfe617ba758f72fc4dd014
di8ry <d.nosov1005@mail.ru>
2022-04-25 22:46:29 +0700

    new one

---
+++ brain_games/games/calc.py
@@ -0,0 +1,28 @@
+import random
+
+
+description_calc = 'What is the result of the expression?'
+
+
+def get_calc():
+    print(description_calc)
+    for attemps in range(3):
+        number_1 = random.randint(1, 20)
+        number_2 = random.randint(1, 20)
+        operation_list = ["+", "-", "*"]
+        operation = random.choice(operation_list)
+        if operation == '+':
+            right_answer = number_1 + number_2
+        elif operation == '-':
+            right_answer = number_1 - number_2
+        elif operation == '*':
+            right_answer = number_1 * number_2
+        print('Question: ', number_1, operation, number_2)
+        print('Your answer: ')
+        user_answer = int(input())
+        if user_answer == right_answer:
+            print('Correct!')
+        else:
+            print(user_answer, 'is wrong answer ;(. Correct answer was', right_answer, "\nLet's try again, Sam!")
+            break
+    print('Game over')
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from brain_games.games.calc import get_calc
+from brain_games.games.even import welcome_user
+
+
+def main():
+    welcome_user()
+    get_calc()
+
+
+
+
+if __name__ == '__main__':
+    main()
