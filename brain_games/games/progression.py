e18c620b3be689855c83db7aa9f3edbfd30a3b6c
di8ry <d.nosov1005@mail.ru>
2022-06-01 23:00:33 +0700

    123

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,7 +8,6 @@ def get_question_and_answer():
     step = random.randint(1, 4)
     progression_range = 20
     progression = list(range(start_number, progression_range, step))
-    answer = random.choice(progression)
     hidden_index = random.randrange(0, len(progression))
     answer, progression[hidden_index] = progression[hidden_index], '..'
     question = ' '.join(map(str, progression))
